1483221097cbc7e9edefc35fff4266085369e782
Thomas Loubiou <xabufr@gmail.com>
2014-05-21 14:52:18 +0200

    Initial

---
+++ my-elasticsearch/attributes/customize.rb
@@ -0,0 +1,2 @@
+normal[:elasticsearch][:version] = '1.1.0'
+normal.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['version'] = '2.1.1'
---
+++ my-elasticsearch/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: my-elasticsearch
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'elasticsearch::default'
