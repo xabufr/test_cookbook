003a34f2271759245726190ad85355ab938a531b
Thomas Loubiou <xabufr@gmail.com>
2014-07-23 14:05:25 +0200

    Tentative d'ajout d'un mapping par défaut

--- my-elasticsearch/recipes/default.rb
+++ my-elasticsearch/recipes/default.rb
@@ -8,3 +8,11 @@
 #
 
 include_recipe 'elasticsearch::default'
+
+template "whois_mapping.json" do
+	path "#{node.elasticsearch[:path][:conf]}/mappings/_default/whois.json"
+	source "whois_mapping.json.erb"
+	owner node.elasticsearch[:user] and group node.elasticsearch[:user] and mode 0755
+	
+	notifies :restart, 'service[elasticsearch]' unless node.elasticsearch[:skip_restart]
+end
