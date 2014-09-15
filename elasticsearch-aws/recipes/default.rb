f2e0249a5312201846910a0d671928b89627b854
Thomas Loubiou <thomas.loubiou@yahoo.com>
2014-09-15 11:09:47 +0200

    Update default.rb

--- elasticsearch-aws/recipes/default.rb
+++ elasticsearch-aws/recipes/default.rb
@@ -9,7 +9,13 @@
 
 include_recipe 'aws'
 
+::Chef::Recipe.send(:include, Opscode::Aws::Ec2)
+
+creds = query_role_credentials
+
 aws_s3_file "/etc/key.pem" do
   bucket "keepalert"
   remote_path "applications/ssl/elasticsearch/key.pem"
+  aws_access_key_id creds['AccessKeyId']
+  aws_secret_access_key creds['SecretAccessKey']
 end
