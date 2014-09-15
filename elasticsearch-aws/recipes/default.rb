911da296e759627280ff966b2a954d53e188bb8c
Thomas Loubiou <xabufr@gmail.com>
2014-09-15 16:18:46 +0200

    Correction user/password

--- elasticsearch-aws/recipes/default.rb
+++ elasticsearch-aws/recipes/default.rb
@@ -30,10 +30,7 @@ end
 ruby_block "add users to passwords file" do
   require 'webrick/httpauth/htpasswd'
   @htpasswd = WEBrick::HTTPAuth::Htpasswd.new("/usr/local/etc/elasticsearch/passwords")
-  node.elasticsearch[:users].each do |u|
-    Chef::Log.debug "Adding user '#{u['username']}'\n"
-    @htpasswd.set_passwd( 'Elasticsearch', u['username'], u['password'] )
-  end
+  @htpasswd.set_passwd( 'Elasticsearch', node[:es_user], node[:es_password] )
   @htpasswd.flush
 end
 
