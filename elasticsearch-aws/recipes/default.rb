378727c5df64c14d9a8aac9fdf447e8538d865ac
Thomas Loubiou <thomas.loubiou@yahoo.com>
2014-09-15 12:46:08 +0200

    Update default.rb

--- elasticsearch-aws/recipes/default.rb
+++ elasticsearch-aws/recipes/default.rb
@@ -10,8 +10,8 @@
 include_recipe 'aws'
 
 aws_s3_file "/etc/key.pem" do
-  bucket "keepalert"
-  remote_path "applications/ssl/elasticsearch/key.pem"
+  bucket "commoncrawl-tloubiou"
+  remote_path "whois/aws-phantom.sh"
   aws_access_key_id node[:custom_access_key]
   aws_secret_access_key node[:custom_secret_key]
 end
