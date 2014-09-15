432ab5014ece82254e648b582b40eca9a74d42c4
Thomas Loubiou <xabufr@gmail.com>
2014-09-15 15:50:37 +0200

    Tentative ajout users

--- elasticsearch-aws/recipes/default.rb
+++ elasticsearch-aws/recipes/default.rb
@@ -27,8 +27,21 @@ aws_s3_file "/etc/ssl/certs/elasticsearch.pem" do
   aws_secret_access_key node[:custom_secret_key]
 end
 
+ruby_block "add users to passwords file" do
+  require 'webrick/httpauth/htpasswd'
+  @htpasswd = WEBrick::HTTPAuth::Htpasswd.new(node.elasticsearch[:path][:conf]}/passwords)
+  node.elasticsearch[:users].each do |u|
+    @htpasswd.set_passwd( 'Elasticsearch', u['username'], u['password'] )
+  end
+  @htpasswd.flush
+end
+
 nginx_proxy "elasticsearch.keepalert.com" do
   ssl_key "elasticsearch"
   port 9200
   cookbook "elasticsearch-aws"
+  custom_config [
+	  "auth_basic           \"Login required\";",
+	  "auth_basic_user_file " + node.elasticsearch[:path][:conf]} + "/passwords;" 
+  ]
 end
